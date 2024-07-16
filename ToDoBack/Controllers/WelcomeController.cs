efa4566bf445bd4e0e1110cee35f8d7ada3cfd27
Atiqur Rahman <atiqurrahman.ofc@gmail.com>
2024-07-16 14:26:15 +0600

    Add project files.

---
+++ ToDoBack/Controllers/WelcomeController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ToDoBack.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class WelcomeController : ControllerBase
+    {
+        public WelcomeController()
+        {
+
+        }
+
+        [HttpGet]
+        public async Task<string> Get()
+        {
+            return $"The ToDo Welcome";
+        }
+    }
+}
